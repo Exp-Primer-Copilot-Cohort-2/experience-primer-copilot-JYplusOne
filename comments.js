07fbe59209a6cbc6066b902d20adc338745a04e3
JYplusOne <142688858+JYplusOne@users.noreply.github.com>
2024-07-08 08:55:29 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 
-// Create web server
-// Create a web server that listens on port 3000 and serves the following HTML file. The final file should be in a file called comment.js.
-<!DOCTYPE html>
-<html>
-    <body>
-        <h1>I am a header</h1>
-        <p>I am a paragraph.</p>
-    </body>
-</html>
-// The server should listen on port 3000.
-
-
 const server = http.createServer((req, res) => {
     fs.readFile('./comment.html', (err, data) => {
         if (err) {
